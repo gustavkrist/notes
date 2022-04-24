589d3e2a0054b2c00c751200ad2789fcc39f60e7
Gustav Kristensen <gustavkrist@gmail.com>
2022-04-25 00:25:05 +0200

    integrating mkdocs + azure

---
+++ docs/javascripts/katex.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+
+  var katexMath = (function() {
+    var maths = document.querySelectorAll('.arithmatex'),
+      tex;
+
+    const macros = {
+      '\\x1n': 'x_1, \\ldots, x_n',
+      '\\X1n': 'X_1, \\ldots, X_n',
+      '\\mean': '\\overline{#1}',
+      '\\med': '\\operatorname{Med}',
+      '\\cov': '\\operatorname{Cov}',
+      '\\var': '\\operatorname{Var}',
+      '\\abs': '\\lvert #1 \\rvert',
+      '\\given': '\\,\\vert\\,'
+    }
+
+    for (var i = 0; i < maths.length; i++) {
+      tex = maths[i].textContent || maths[i].innerText;
+      if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false, 'globalGroup': true, macros });
+      } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+        katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true, 'globalGroup': true, macros });
+      }
+    }
+  });
+
+  (function() {
+    var onReady = function onReady(fn) {
+      if (window.document$) {
+        window.document$.subscribe(fn)
+      } else if (document.addEventListener) {
+        document.addEventListener("DOMContentLoaded", fn);
+      } else {
+        document.attachEvent("onreadystatechange", function() {
+          if (document.readyState === "interactive") {
+            fn();
+          }
+        });
+      }
+    };
+
+    onReady(function() {
+      if (typeof katex !== "undefined") {
+        katexMath();
+      }
+    });
+  })();
+
+}());
